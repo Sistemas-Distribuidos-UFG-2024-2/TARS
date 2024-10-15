37c727b2df70cacdaa330dbc50bb85d712e1d3b9
Maria Eduarda <87625092+maririchii@users.noreply.github.com>
2024-10-15 14:26:44 -0300

    Deleted fuction

--- 18-10/socket/ideal_weight.py
+++
@@ -1,9 +0,0 @@
-def define_ideal_weight(height, gender):
-
-    # Calculando peso ideal
-    if gender == "M":
-        return (72.7 * height) - 58
-    elif gender == "F":
-        return (62.1 * height) - 44.7
-    else:
-        return -1
