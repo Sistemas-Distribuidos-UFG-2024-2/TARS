e112ca238ea7fa9cda9967263820bb5d04ed6577
gusneves <gustavonpl@protonmail.com>
2024-11-08 15:57:50 -0300

    Added acceleration consumer and added hostname to docker-compose common env

--- SpaceshipMonitoringSystem/AnalysisService/Consumers/AccelerationConsumer.cs
+++ SpaceshipMonitoringSystem/AnalysisService/Consumers/AccelerationConsumer.cs
@@ -1,6 +1,21 @@
+using AnalysisService.DTO;
+using MassTransit;
+
 namespace AnalysisService.Consumers;
 
-public class AccelerationConsumer
+public class AccelerationConsumer : IConsumer<AccelerationMessage>
 {
-    
+    private readonly ILogger<AccelerationConsumer> _logger;
+
+    public AccelerationConsumer(ILogger<AccelerationConsumer> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task Consume(ConsumeContext<AccelerationMessage> context)
+    {
+        _logger.LogInformation("Acceleration: {Acceleration}", context.Message.Acceleration);
+
+        return Task.CompletedTask;
+    }
 }
--- SpaceshipMonitoringSystem/AnalysisService/DTO/GyroscopeMessage.cs
+++ SpaceshipMonitoringSystem/AnalysisService/DTO/GyroscopeMessage.cs
@@ -1,8 +1,3 @@
 namespace AnalysisService.DTO;
 
-public class GyroscopeMessage
-{
-    public double X { get; set; }
-    public double Y { get; set; }
-    public double Z { get; set; }
-}
+public record GyroscopeMessage(double X, double Y, double Z);
